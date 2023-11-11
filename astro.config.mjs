98e2aea8438580d7641b5a0ce75af7912feca2d6
Brent Arias <barias@sphereoi.com>
2023-11-10 22:53:24 -0600

    Nav update, Sanity CMS blog, Astro v3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import sanity from "@sanity/astro";
+const projectId = "w5d9vy16";
+const dataset = "blog";
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()]
-	// ,server: {
-	//   port: 4321,
-	//   host: true
-	// }   
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), sanity({
+      projectId,
+      dataset,
+      useCdn: false
+    })]
+  // ,server: {
+  //   port: 4321,
+  //   host: true
+  // }   
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,22 @@
+import { type Image } from 'sanity';
+
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'SOUL SOVEREIGNTY';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export type Post = {
+	body: {
+		title: string;
+	}
+	publishedAt: string,
+	_updatedAt: string,
+	pubDate: Date,
+	updatedDate?: Date,
+	title: string,
+	slug: {
+		current: string
+	},
+	mainImage: Image
+}
