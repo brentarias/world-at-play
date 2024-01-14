e36b0728b0e3c5dab5dba936253a26208573c622
Brent Arias <barias@sphereoi.com>
2024-01-13 23:26:57 -0600

    Add social media icons/links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import sanity from "@sanity/astro";
+import icon from "astro-icon";
 const projectId = "w5d9vy16";
 const dataset = "blog";
 
@@ -10,11 +10,15 @@ const dataset = "blog";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soulsovereignty.earth',
-  integrations: [mdx(), sitemap(), sanity({
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    sanity({
       projectId,
       dataset,
       useCdn: false
-    })]
+    }),
+    icon({iconDir: "src/icons"})]
   // ,server: {
   //   port: 4321,
   //   host: true
